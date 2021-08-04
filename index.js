d5003980aa7f741d1eff00af16cacd364a128e2a
Ali Madooei <alimadooei@gmail.com>
2021-08-04 12:12:19 -0400

    Update the app so when a comment is made on a push commit, if the comment contains `@grade-bot` annotation, it would reply and acknowledge it received the message.

--- index.js
+++ index.js
@@ -7,10 +7,24 @@ module.exports = (app) => {
   app.log.info("Yay, the app was loaded!");
 
   app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
+    console.log("Recived: issues.opened");
+    const response = context.issue({
       body: "Thanks for opening this issue!",
     });
-    return context.octokit.issues.createComment(issueComment);
+    console.log(response);
+    return context.octokit.issues.createComment(response);
+  });
+
+  app.on("commit_comment.created", async (context) => {
+    console.log("Received: commit_comment.created");
+    const body = context.payload.comment.body;
+    if (body.includes("@grade-bot")) {
+      const response = context.repo({
+        body: "I acknowledge the reception of your comment!",
+        commit_sha: context.payload.comment.commit_id
+      });
+      return context.octokit.repos.createCommitComment(response);
+    }
   });
 
   // For more information on building apps:
